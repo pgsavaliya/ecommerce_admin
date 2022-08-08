57ad2ac31b986605917b12d28ed045120e74b735
pgsavaliya <pavansavaliya.itcodehelp@gmail.com>
2022-08-08 13:50:10 +0530

    dfs

--- service/admin/sell/viewSellService.js
+++ service/admin/sell/viewSellService.js
@@ -6,8 +6,8 @@ import downloadsFolder from "downloads-folder";
 import mongoose from "mongoose";
 
 export async function viewSellService(input, res) {
-  // return new Promise(async (resole, reject) => {
-  //   try {
+  return new Promise(async (resole, reject) => {
+    try {
   // console.log(downloadsFolder());
 
   let reasult = [];
@@ -43,47 +43,15 @@ export async function viewSellService(input, res) {
   }
 
   if (reasult != "") {
-    reasult = reasult.map((item) => {
-      return {
-        ...item,
-        Sale_Date: item.createdAt
-          .toISOString()
-          .replace(/T/, " ")
-          .replace(/\..+/, ""),
-      };
-    });
-    let pdfDoc = new Pdfdoc({
-      // layout: "landscape",
-      // margin: 50,
-      // size: "a4",
-    });
-    pdfDoc.pipe(fs.createWriteStream("./pdf/Pavan.pdf"));
-    const table = {
-      title: "E-Comm  ItCodeHelp",
-      subtitle: "Subtitle",
-      headers: [
-        { label: "Order_Net", property: "Order_Net" },
-        { label: "Full_Name", property: "Full_Name" },
-        { label: "Buyer", property: "Buyer" },
-        { label: "Sale_Date", property: "Sale_Date" },
-      ],
-      datas: reasult,
-    };
-    await pdfDoc.table(table, {
-      // width: 700,
-      border: 1,
-    });
-    pdfDoc.end();
-    // ./router/v1.js
-    // console.log()
-    res.dowland("./Pavan.pdf", function (error) {
-      console.log("Error : ", error);
-    });
-    //   } else {
-    //     reject("Sell Not Found");
-    //   }
-    // } catch (e) {
-    //   reject("Something is worng" + e);
+    resole(reasult)
   }
-  // });
+  else{
+    reject("Sell Not Found")
+  }
+}
+  catch (e) {
+    reject("Something is worng" + e);
+  }
+});
+  
 }
