2aaadc125c13b699477434cbaa62291c50b0d3ca
pgsavaliya <pavansavaliya.itcodehelp@gmail.com>
2022-08-08 13:56:30 +0530

    dfs

--- service/admin/sell/viewSellService.js
+++ service/admin/sell/viewSellService.js
@@ -8,50 +8,47 @@ import mongoose from "mongoose";
 export async function viewSellService(input, res) {
   return new Promise(async (resole, reject) => {
     try {
-  // console.log(downloadsFolder());
+      // console.log(downloadsFolder());
 
-  let reasult = [];
-  if (input.timming == 0) {
-    reasult = await viewSellModel.aggregate([
-      {
-        $match: {
-          accId: mongoose.Types.ObjectId(input.accId),
-          Full_Name: {
-            $regex: input.search,
-            $options: "i",
+      let reasult = [];
+      if (input.timming == 0) {
+        reasult = await viewSellModel.aggregate([
+          {
+            $match: {
+              accId: mongoose.Types.ObjectId(input.accId),
+              Full_Name: {
+                $regex: input.search,
+                $options: "i",
+              },
+            },
           },
-        },
-      },
-    ]);
-  } else {
-    let date1 = new Date();
-    date1.setMonth(date1.getMonth() - input.timming);
-    reasult = await viewSellModel.aggregate([
-      {
-        $match: {
-          accId: mongoose.Types.ObjectId(input.accId),
-          createdAt: {
-            $gte: date1,
+        ]);
+      } else {
+        let date1 = new Date();
+        date1.setMonth(date1.getMonth() - input.timming);
+        reasult = await viewSellModel.aggregate([
+          {
+            $match: {
+              accId: mongoose.Types.ObjectId(input.accId),
+              createdAt: {
+                $gte: date1,
+              },
+              Full_Name: {
+                $regex: input.search,
+                $options: "i",
+              },
+            },
           },
-          Full_Name: {
-            $regex: input.search,
-            $options: "i",
-          },
-        },
-      },
-    ]);
-  }
+        ]);
+      }
 
-  if (reasult != "") {
-    resole(reasult)
-  }
-  else{
-    reject("Sell Not Found")
-  }
-}
-  catch (e) {
-    reject("Something is worng" + e);
-  }
-});
-  
+      if (reasult != "") {
+        resole(reasult);
+      } else {
+        reject("Sell Not Found");
+      }
+    } catch (e) {
+      reject("Something is worng" + e);
+    }
+  });
 }
